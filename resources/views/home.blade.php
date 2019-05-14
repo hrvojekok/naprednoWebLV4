dc2cf82413922c39ce0db70cc827b7ce3cf363a1
Hrvoje Kokosarevic <hrvoje.kok@gmail.com>
2019-05-14 14:41:14 +0200

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Popis taskova</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -15,6 +15,22 @@
                     @endif
 
                     You are logged in!
+
+                    <br><br>
+                    <?php
+                      $tasks = DB::table('tasks')->pluck('naziv_rada');
+                      $size = sizeof($tasks);
+                      //print($tasks);
+                      if($tasks != ""){
+                        for($i=0; $i<$size; $i++){
+                            print($tasks[$i]);
+                            print("<br>");
+                        }
+                      } else {
+                        print("No Tasks in database");
+                      }
+
+                     ?>
 
                     <div class="links"><br>
                           <a href="/roles">Select yout role</a>
